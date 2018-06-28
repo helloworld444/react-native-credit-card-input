1fc1794f297e54bbe0a3644f09c0a91101660319
Andrey Saleba <andrey.saleba@gmail.com>
2018-06-28 13:40:53 +0300

    transferred styles from app

---
+++ src/colors.js
@@ -0,0 +1,29 @@
+const colors = {
+  background: '#DADCCF',
+  darkGrey: '#2A3F50',
+  facebook: '#3B5999',
+  goggle: '#E54F38',
+  lightAsh: '#E7E7E7',
+  white: 'rgb(255,255,255)',
+  lightGrey: '#a8a9a9',
+  greyWhite: '#dfe1d5',
+  transperant: 'transparent',
+  creamyWhite: '#E0E2D4',
+  reddish: 'red',
+  navyBlue: '#293E4F',
+  oceanBlue: '#2680BE',
+  navyDarkBlue: '#1c396b',
+  blue: '#286ca6',
+  brightBlue: '#007dff',
+  black: 'black',
+  thinGrey: '#dadada',
+  letterBlack: '#4a4a4a',
+  pureBlue: '#1772CA',
+  ash: '#f0f0f0',
+  transparent: 'transparent',
+  lightWhite: '#f6f6f6',
+  letterGrey: '#b6b6b6',
+  lightlightGrey: '#ebebeb'
+};
+
+export default colors;
